bd8cca53171702d2cc89e90cb939e23f98413f59
Sean Gilleece <47053602+Gilleece@users.noreply.github.com>
2023-01-10 17:00:48 +0000

    Added ingredient model

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,10 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'A love test description', 'https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg')
+    new Recipe('Test Recipe', 'A love test description', 'https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg'),
+    new Recipe('Test Recipe', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt/', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1280px-Test-Logo.svg.png'),
+    new Recipe('Test Recipe', 'A love test description', 'https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg'),
+    new Recipe('Test Recipe', 'A love test description', 'https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg'),
   ];
   constructor() {
   }
